74215fe8f9ccf95d8436334b4105b5aef082e32a
Abir-SAAD <abir.saad@hotmail.fr>
2022-03-12 15:14:38 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import logo from "./logo.svg";
+import "./App.css";
+import Capture from "./Capture.png";
+import React from "react";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 10px  black", maxWidth: "100vW" }}>
+        <h1 className="title red">Nature</h1>
+        <br />
+        <img src={Capture} alt="Capture" />
+        <br />
+        <img src="/photo.jpg" alt="photo" />
+        <br />
+        <iframe
+          width="500"
+          height="280"
+          src="https://www.youtube.com/embed/d2SNX3bfYKw"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
     </div>
   );
 }
---
+++ src/img/file.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "./";
+
+function file() {
+  return <div>file</div>;
+}
+
+export default file;
